80f82793fb25c1b015fc7ae7b890b07d023a11be
isaacdong88 <isaacdong88@gmail.com>
2023-03-31 11:24:32 -0400

    can view a list of all pokemon in browser

--- Week11/Lab/pokemon_app/server.js
+++ Week11/Lab/pokemon_app/server.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/pokemon", (req, res) => {
-  res.render("Index");
+  res.render("Index", { pokemon: pokemon });
 });
 
 app.listen(PORT, () => {
--- Week11/Lab/pokemon_app/views/Index.jsx
+++ Week11/Lab/pokemon_app/views/Index.jsx
@@ -1,14 +1,26 @@
 const React = require('react');
 const myStyle = {
-    color: '#ffffff',
-    backgroundColor: '#000000'
+    color: 'black',
+    // backgroundColor: '#000000'
+    'text-transform': 'capitalize'
 };
 
 class Index extends React.Component {
   render() {
+    const {pokemon} = this.props;
     return (
       <div style={myStyle}>
         <h1>See All The Pokemon</h1>
+        <ul>
+            {pokemon.map((poke,i)=>{
+                return(
+                    <li key={i}>
+                    {poke.name} <br />
+                    <img src={poke.img} alt="Poke" />
+                </li>
+                )
+            })}
+        </ul>
       </div>
     )
   }
