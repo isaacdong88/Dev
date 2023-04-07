9d0315cbb5cbd662be675dc556f58d0625c97dbd
isaacdong88 <isaacdong88@gmail.com>
2023-04-07 11:18:24 -0400

    add create route

--- Week12/Lab/captains_log/server.js
+++ Week12/Lab/captains_log/server.js
@@ -15,8 +15,38 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", (req, res) => {
+mongoose.set("strictQuery", true);
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("connected to mongo");
+});
+
+app.get("/logs/new", (req, res) => {
   res.render("New");
+});
+
+app.post("/logs", (req, res) => {
+  if (req.body.shipIsBroken === "on") {
+    //if checked, req.body.readyToEat is set to 'on'
+    req.body.shipIsBroken = true; //do some data correction
+  } else {
+    //if not checked, req.body.readyToEat is undefined
+    req.body.shipIsBroken = false; //do some data correction
+  }
+  res.send(req.body);
+  //   Fruit.create(req.body, (error, createdFruit) => {
+  //     // res.send(createdFruit);
+  //     res.redirect("/fruits");
+  //   });
+
+  //   fruits.push(req.body);
+  //   console.log(fruits);
+  //   res.send("data received");
+  //   res.redirect("/fruits");
 });
 
 app.listen(PORT, () => {
